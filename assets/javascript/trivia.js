cd90024c40e74481b92979f747a5c4510a4464a1
Nigel John <nigeljohn@Nigels-MacBook-Pro.local>
2016-05-03 00:20:36 -0400

    Shell with first question

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,76 @@
+var Trivia = {
+	correct: 0,
+	incorrect: 0,
+	unanswered: 0,
+};
+
+$(document).ready(function(){
+	$('.wella1, .wella2, .wella3, .wella4').addClass('hoverOut');
+	$('button').addClass('hoverOut');
+
+	$('button').hover(function(){
+		$('button').addClass('hoverIn').removeClass('hoverOut');
+	}, function(){
+		$('button').removeClass('hoverIn').addClass('hoverOut');
+	});
+
+	$('.wella1').hover(function(){
+		$('.wella1').addClass('hoverIn').removeClass('hoverOut');
+	}, function(){
+		$('.wella1').removeClass('hoverIn').addClass('hoverOut');
+	});
+
+	$('.wella2').hover(function(){
+		$('.wella2').addClass('hoverIn').removeClass('hoverOut');
+	}, function(){
+		$('.wella2').removeClass('hoverIn').addClass('hoverOut');
+	});
+
+	$('.wella3').hover(function(){
+		$('.wella3').addClass('hoverIn').removeClass('hoverOut');
+	}, function(){
+		$('.wella3').removeClass('hoverIn').addClass('hoverOut');
+	});
+
+	$('.wella4').hover(function(){
+		$('.wella4').addClass('hoverIn').removeClass('hoverOut');
+	}, function(){
+		$('.wella4').removeClass('hoverIn').addClass('hoverOut');
+	});
+
+	$('.well-sm').click(function(){
+		
+	});
+question1();
+});
+
+function question1(){
+	$('.wellq1').empty().append('<div>Which of the following type of variable is visible everywhere in your JavaScript code?</div>');
+	$('.wella1').empty().append('<button class="right" value="yes">Global variable</button>');
+	$('.wella2').empty().append('<button value="no">Local variable</button>');
+	$('.wella3').empty().append('<button value="no">Both of the above</button>');
+	$('.wella4').empty().append('<button value="no">None of the above</button>');
+
+	
+$('button').click(function(){
+	var x = $(this);
+	if(x.val() === "yes"){
+		//correct++;
+		console.log(this.correct);
+		$('.answer').empty().append('Correct!!!');
+	}
+	else if(x.val() === 'no'){
+		//incorrect++;
+		console.log(this.incorrect);
+		$('.answer').empty().append('Nope!<br>The correct answer was: ' + $('button.right').text());	
+	}
+	else{
+		//unanswered++;
+		console.log(this.unanswered);
+	}
+});
+
+
+
+}
+
